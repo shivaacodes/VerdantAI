a9d22f802ed50576b7ffceb5bd8d619bb38dd110
Shiva Sajay <shivasajay007@gmail.com>
2026-05-21 20:25:47 +0530

    refactor(layout): de-clutter main landing page to isolate console app route

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,17 +1,15 @@
 import HeroSection from "@/components/hero-section";
-import MarqueeSection from "@/components/marquee-section";
 import FeatureSection from "@/components/features-section";
-import AnalyzeSection from "@/components/analyze-section";
 import About from "@/components/about";
+import Footer from "@/components/footer";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
-      <MarqueeSection />
       <FeatureSection />
-      <AnalyzeSection />
       <About />
+      <Footer />
     </>
   );
 }
